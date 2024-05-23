3b96d2a06f22a0edc33e12c70316e04adc8f38a9
saraalsheikh <sara.alsheikh01@gmail.com>
2024-05-23 14:22:06 +0200

    Created game class

---
+++ game.py
@@ -0,0 +1,10 @@
+from src.player import Player
+from src.dice import DiceHand
+from src.highscore import HighScore
+from src.intelligence import Intelligence
+
+
+class Game:
+    """
+    Represents the game logic and state.
+    """
