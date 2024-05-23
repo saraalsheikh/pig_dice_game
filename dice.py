97aac5a8fb404577e7be6bd5b9b2079d3cd1ad9a
saraalsheikh <sara.alsheikh01@gmail.com>
2024-05-23 14:19:32 +0200

    Created DiceHand class

--- dice.py
+++ dice.py
@@ -17,3 +17,21 @@ class Dice:
         Rolls the die and returns a random value.
         """
         return random.randint(1, self.faces)
+    
+
+class DiceHand:
+    """
+    Represents a hand of dice.
+    """
+
+    def __init__(self, num_dice=2):
+        """
+        Initializes a hand with a given number of dice.
+        """
+        self.dice = [Dice() for _ in range(num_dice)]
+
+    def roll_all(self):
+        """
+        Rolls all dice in the hand and returns the results.
+        """
+        return [dice.roll() for dice in self.dice]
