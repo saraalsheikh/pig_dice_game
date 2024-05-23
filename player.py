fe8545813d52dde72d78a36ef842877b4485aff9
saraalsheikh <sara.alsheikh01@gmail.com>
2024-05-23 15:27:39 +0200

    Added init function

--- player.py
+++ player.py
@@ -2,3 +2,11 @@ class Player:
     """
     Represents a player in the game.
     """
+
+def __init__(self, name):
+        """
+        Initializes a new player with a name and a score of zero.
+        """
+        self.name = name
+        self.score = 0
+
