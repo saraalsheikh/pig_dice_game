fd15650ce33899d4a48603b7d4722e9c96ce3787
saraalsheikh <sara.alsheikh01@gmail.com>
2024-05-23 15:21:35 +0200

    wip

--- intelligence.py
+++ intelligence.py
@@ -2,3 +2,9 @@ class Intelligence:
     """
     Manages the decision-making logic for the computer player.
     """
+
+    def __init__(self, level='easy'):
+        """
+        Initializes the intelligence level for the computer player.
+        """
+        self.level = level
