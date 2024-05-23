3549f6a843b67dca0e8409e038abbeef6e4e36ef
saraalsheikh <sara.alsheikh01@gmail.com>
2024-05-23 15:01:24 +0200

    Added load_scores function

--- highscore.py
+++ highscore.py
@@ -14,3 +14,11 @@ def __init__(self, filename='highscores.json'):
         self.filename = filename
         self.scores = self.load_scores()
 
+def load_scores(self):
+        """
+        Loads scores from a file.
+        """
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r') as file:
+                return json.load(file)
+        return {}
