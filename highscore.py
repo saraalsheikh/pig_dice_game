fca363bdbf056f769048d20fe06018ab5762cd1f
saraalsheikh <sara.alsheikh01@gmail.com>
2024-05-23 14:59:25 +0200

    Added init function

--- highscore.py
+++ highscore.py
@@ -7,4 +7,10 @@ class HighScore:
     Manages the high scores for the game.
     """
 
-   
+def __init__(self, filename='highscores.json'):
+        """
+        Initializes the high score manager with a file to store scores.
+        """
+        self.filename = filename
+        self.scores = self.load_scores()
+
